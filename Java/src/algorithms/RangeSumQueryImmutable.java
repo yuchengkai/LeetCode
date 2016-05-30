96e30f3eacb112dcf97cc9cbe9024c7d466b6d34
yuchengkai <yycckk0@gmail.com>
2016-05-30 21:55:28 +0800

    ad balanced binary tree

---
+++ Java/src/algorithms/RangeSumQueryImmutable.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+import leetcode.TreeNode;
+
+
+/**
+ * @author yuchengkai
+ *
+ * @time   2016年5月30日
+ *
+ */
+public class RangeSumQueryImmutable {
+
+	/**
+	 * range_sum(a,b)=sum(b)-sum(a)
+	 * */
+    int []n;
+    public RangeSumQueryImmutable(int[] nums) {
+        n=nums;
+        for(int i=1;i<n.length;i++)
+            n[i]+=n[i-1];
+    }
+
+    public int sumRange(int i, int j) {
+        if(i==0)return n[j];
+        return n[j]-n[i-1];
+    }
+
+}
